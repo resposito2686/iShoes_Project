3756a0773a3f299048489a608aa507d8e1b4c91e
Robert Esposito <rjesposito86@gmail.com>
2021-10-06 23:15:11 -0700

    flask + homepage
    commit includes basic Flask framework and homepage with navigation bar.

---
+++ iShoes.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+image_folder = os.path.join('static', 'images')
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
+    logo_path = os.path.join(image_folder, 'logo.jpg')
+    return render_template("home.html", logo_img=logo_path)
+
+
+if __name__ == '__main__':
+    app.run()
