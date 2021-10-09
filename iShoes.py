8130d9cec1593c14c2e956747c2541cd8bbdf74b
Robert Esposito <44630395+resposito2686@users.noreply.github.com>
2021-10-08 20:22:42 -0700

    Template implemented
    Created a template that can be used for all html pages.
    
    Updated navigation bar links.
    
    Created placeholder html pages.

--- iShoes.py
+++ iShoes.py
@@ -4,6 +4,8 @@ import os
 
 app = Flask(__name__)
 image_folder = os.path.join('static', 'images')
+user_name = 'Login'
+active_page = ''
 
 
 @app.route('/')
@@ -13,9 +15,34 @@ def index():
 
 @app.route('/home')
 def home():
-    # REPLACE LATER WITH USERNAME LOGIC
-    user_name = "Login"
-    return render_template("home.html", user_name=user_name)
+    global active_page, user_name
+
+    active_page = 'home'
+    return render_template("home.html", active=active_page, user_name=user_name)
+
+
+@app.route('/shop')
+def shop():
+    global active_page, user_name
+
+    active_page = 'shop'
+    return render_template("shop.html", active=active_page, user_name=user_name)
+
+
+@app.route('/create')
+def create():
+    global active_page, user_name
+
+    active_page = 'create'
+    return render_template("create.html", active=active_page, user_name=user_name)
+
+
+@app.route('/login')
+def login():
+    global active_page, user_name
+
+    active_page = 'login'
+    return render_template("login.html", active=active_page, user_name=user_name)
 
 
 if __name__ == '__main__':
