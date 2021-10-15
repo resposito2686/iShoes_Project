87b9c89bb41ccd992184e5ef308b0bde2e79856e
Robert Esposito <44630395+resposito2686@users.noreply.github.com>
2021-10-15 01:35:48 -0700

    Update iShoes.py
    added instructions on how to insert data into MySQL database

--- iShoes.py
+++ iShoes.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, redirect, url_for, request
 from flask_mysqldb import MySQL
 import os
+import hashlib
 
 app = Flask(__name__)
 image_folder = os.path.join('static', 'images')
@@ -9,19 +10,18 @@ user_name = 'Login'
 active_page = ''
 cart_empty = True
 
-
-# TODO - install flask_mysqldb by typing 'pip install flask_mysqldb' in your command prompt
-# DATABASE IP ADDRESS
+#:  TODO - install flask_mysqldb by typing 'pip install flask_mysqldb' in your command prompt
+#:  DATABASE IP ADDRESS
 app.config['MYSQL_HOST'] = 'localhost'
 
-# DATABASE USER NAME
+#:  DATABASE USER NAME
 app.config['MYSQL_USER'] = 'root'
 
-# DATABASE PASSWORD
-app.config['MYSQL_PASSWORD'] = ''
+#:  DATABASE PASSWORD
+app.config['MYSQL_PASSWORD'] = 'password123'
 
-# SCHEMA NAME
-app.config['MYSQL_DB'] = ''
+#:  SCHEMA NAME
+app.config['MYSQL_DB'] = 'ishoesdb'
 
 mysql = MySQL(app)
 if mysql:
@@ -32,19 +32,17 @@ else:
 
 @app.route('/')
 def index():
+    #:  EXAMPLE MySQL QUERY
+    #:    connect = mysql.connect()             <-- CURSOR FOR QUERIES
+    #:    cursor.execute("SELECT * from table") <-- QUERY GOES HERE
+    #:    sql_data = cursor.fetchone()          <-- DATA RETURNED AS TUPLE
+
     return redirect(url_for('home'))
 
 
 @app.route('/home')
 def home():
     global active_page, user_name, cart_empty
-
-    ''' 
-    EXAMPLE MySQL QUERY
-        cursor = mysql.connection.cursor()    <-- CURSOR FOR QUERIES
-        cursor.execute("SELECT * from table") <-- QUERY GOES HERE
-        sql_data = cursor.fetchone()          <-- DATA RETURNED AS TUPLE
-    '''
 
     active_page = 'home'
     cart_empty = True
@@ -78,9 +76,46 @@ def login():
 
     if request.method == 'POST':
         user_name = request.form['userNameInput']
-        password = request.form['passwordInput']
-        print(user_name)
-        print(password)
+
+        #:  Password is ran through SHA512 hashing algorithm for security
+        password = hashlib.sha512(request.form['passwordInput'].encode('utf-8')).hexdigest()
+
+        #:  --- How to insert data into a MySQL database ---
+        #:  try block to catch any connection errors that could result in a crash
+        try:
+            #:  cursor used to execute commands
+            cursor = mysql.connection.cursor()
+
+            #:  execute() function arguments are ("""MySQL Query""", (Python variables and literals))
+            #:
+            #:  values are ALWAYS %s, which is placeholder value that will be filled by the Python variables
+            #:  and literals
+            #:
+            #:  Python variables and literals are a Tuple with data indexed in the same order it appears in the MySQL
+            #:  query.
+            cursor.execute("""INSERT into 
+                            users (
+                                addressCity,
+                                addressNum,
+                                addressState,
+                                addressStreet,
+                                addressZip,
+                                emailAddress,
+                                firstName,
+                                lastName,
+                                password,
+                                phoneNumber,
+                                userName)
+                            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                           ('San Diego', '123', 'CA', 'Fake Street', '92115', 'johndoe@gmail.com', 'John', 'Doe',
+                            password, '6195555555', user_name))
+
+            #:  commit changes to MySQL database
+            mysql.connection.commit()
+
+        #:  except block to print any connection errors that may occur.
+        except Exception as e:
+            print('Error insert data in table...' + str(e))
 
     return render_template("login.html", active=active_page, user_name=user_name, cart_empty=cart_empty)
 
